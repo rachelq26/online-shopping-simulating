d0578443dbdd668ac3afa02f372c886303013470
rachelq26 <zhangqiuzi26@gmail.com>
2025-08-16 23:47:29 +0200

    remove unused methods

--- Repositories/OrderRepository.cs
+++ Repositories/OrderRepository.cs
@@ -12,37 +12,4 @@ public class OrderRepository : IOrderRepository
     {
         _context = context;
     }
-
-    public async Task<Order?> GetByIdAsync(int id)
-    {
-        return await _context.Orders
-            .Include(o => o.User)
-            .Include(o => o.Items)
-                .ThenInclude(oi => oi.Product)
-            .FirstOrDefaultAsync(o => o.Id == id);
-    }
-
-    public async Task<IEnumerable<Order>> GetByUserIdAsync(int userId)
-    {
-        return await _context.Orders
-            .Include(o => o.Items)
-                .ThenInclude(oi => oi.Product)
-            .Where(o => o.UserId == userId)
-            .ToListAsync();
-    }
-
-    public async Task<Order> AddAsync(Order order)
-    {
-        _context.Orders.Add(order);
-        await _context.SaveChangesAsync();
-        return order;
-    }
-
-    public async Task<Order> UpdateAsync(Order order)
-    {
-        order.LastUpdated = DateTime.UtcNow;
-        _context.Orders.Update(order);
-        await _context.SaveChangesAsync();
-        return order;
-    }
 }
